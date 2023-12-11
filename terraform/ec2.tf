526c518b3de061a8c184720df9266676b90aaef0
Marcello Alexandre <marcello.alexandre@weni.ai>
2023-12-11 18:20:36 -0300

    Adjust keyname

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ec2_instance" {
   count         = 2
   ami           = "ami-0b6c2d49148000cd5" 
   instance_type = "t2.micro"
-  key_name      = var.key_pair_name
+  key_name      = "MyKeyPair" #var.key_pair_name
 
   vpc_security_group_ids  = [aws_security_group.nginx_sg_tf.id]     
 
