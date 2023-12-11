5850357d00386064fd873ef2bfcfc3f9010e5052
Marcello Alexandre <marcello.alexandre@weni.ai>
2023-12-11 18:29:25 -0300

    Adjust terraform

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,21 +1,21 @@
 # EC2
 resource "aws_instance" "ec2_instance" {
   count         = 2
-  ami           = "ami-0b6c2d49148000cd5" 
+  ami           = "ami-0b6c2d49148000cd5"
   instance_type = "t2.micro"
   key_name      = "MyKeyPair" #var.key_pair_name
 
-  vpc_security_group_ids  = [aws_security_group.nginx_sg_tf.id]     
+  vpc_security_group_ids = [aws_security_group.nginx_sg_tf.id]
 
   tags = {
     Name = "nginx-${count.index + 1}"
   }
 
-  user_data     = file("userdata.tpl")
+  user_data = file("userdata.tpl")
 }
 
 data "aws_vpc" "default" {
- default = true
+  default = true
 }
 
 resource "aws_security_group" "nginx_sg_tf" {
@@ -24,23 +24,23 @@ resource "aws_security_group" "nginx_sg_tf" {
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,15 +3,15 @@ resource "aws_s3_bucket" "static_website" {
   bucket = "ittalent-terraform-23"
   tags = {
     Name = "bucket static website"
-  } 
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "static_website" {
   bucket = aws_s3_bucket.static_website.id
 
-  block_public_acls = false
-  block_public_policy = false
-  ignore_public_acls = false
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
@@ -31,17 +31,17 @@ resource "aws_s3_bucket_ownership_controls" "static_website" {
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
-  
+
 }
 
 resource "aws_s3_bucket_acl" "static_website" {
-  depends_on = [ 
-    aws_s3_bucket_ownership_controls.static_website, 
+  depends_on = [
+    aws_s3_bucket_ownership_controls.static_website,
     aws_s3_bucket_public_access_block.static_website,
   ]
-  
+
   bucket = aws_s3_bucket.static_website.id
-  acl = "public-read"
+  acl    = "public-read"
 }
 
 
@@ -56,18 +56,18 @@ locals {
 }
 
 resource "aws_s3_object" "static_website_files" {
-  for_each = fileset("../site/", "*")
-  bucket = aws_s3_bucket.static_website.id
-  key = each.value
-  source = "../site/${each.value}"
+  for_each         = fileset("../site/", "*")
+  bucket           = aws_s3_bucket.static_website.id
+  key              = each.value
+  source           = "../site/${each.value}"
   content_type     = lookup(local.content_types, element(split(".", each.value), length(split(".", each.value)) - 1), "text/plain")
   content_encoding = "utf-8"
 
-  acl = "public-read"
-  depends_on = [ aws_s3_bucket_acl.static_website ]
+  acl        = "public-read"
+  depends_on = [aws_s3_bucket_acl.static_website]
 }
 
 output "website_url" {
   value = "http://${aws_s3_bucket.static_website.bucket}.s3-website-sa-east-1.amazonaws.com"
-  
+
 }
